9c87ed4aef9aa12b4ae709abead7c960e29c217d
SovietSpy2 <sovietspy2@yandex.com>
2019-02-05 21:18:52 +0100

    api connected successfully

--- src/containers/list.js
+++ src/containers/list.js
@@ -7,19 +7,30 @@ export default class List extends PureComponent {
     constructor(props) {
         super(props);
         this.state= {
-            items: []
+            items: [],
+            text:'old text'
         };
     }
 
     render() { 
         return (<div> 
             <Paper className="page">
-            <Button variant="contained" color="primary">fancy button</Button>
-                hello
+                <Button variant="contained" color="primary" onClick={this.handleClick}>fancy button</Button>
+                <p>{this.state.text}</p>
             </Paper>
-             
-
             </div>)
+    }
+
+    handleClick = async e => {
+        e.preventDefault();
+            const response = await fetch('/home', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            });
+            const body = await response.text();
+            this.setState({ text: body });
     }
 
     renderButton() { 
